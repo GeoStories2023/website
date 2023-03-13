808176b6d7c66a88ac08e4a6206619ab6319a273
Noah4ever <noah.thiering@web.de>
2023-03-13 15:14:53 +0100

    Fix country view

--- src/components/CountryView/CountryView.jsx
+++ src/components/CountryView/CountryView.jsx
@@ -4,22 +4,4 @@ function CountryView() {
   return <div>CountryView</div>;
 }
 
-export default CountryView; "react";
-
-function CountryView() {
-  return <div>CountryView</div>;
-}
-
-export default CountryView; "react";
-
-function CountryView() {
-  return <div>CountryView</div>;
-}
-
-export default CountryView; "react";
-
-function CountryView() {
-  return <div>CountryView</div>;
-}
-
 export default CountryView;
