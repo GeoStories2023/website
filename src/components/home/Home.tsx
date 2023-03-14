8343063d0117992d75533fb6ac214ed631c2e439
Noah4ever <noah.thiering@web.de>
2023-03-14 11:37:23 +0100

    Change feature preview

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -31,23 +31,34 @@ function Home() {
         </div>
       </div>
       <div className="quote-container">
-        <div className="quote-text">
-          <div className="quote-quote">
-            <span>
-              "Die Welt ist ein Buch. Wer nie reist, sieht nur eine Seite
-              davon."
-            </span>
+        <svg
+          className="quote-wave"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path d="M0,128L120,149.3C240,171,480,213,720,224C960,235,1200,213,1320,202.7L1440,192L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+        </svg>
+        <div className="quote-inner-container">
+          <div className="quote-text">
+            <div className="quote-quote">
+              <span>
+                "Die Welt ist ein Buch. Wer nie reist, sieht nur eine Seite
+                davon."
+              </span>
+            </div>
+            <div className="quote-author">
+              <span>Augustinus Aurelius</span>
+            </div>
           </div>
-          <div className="quote-author">
-            <span>Augustinus Aurelius</span>
-          </div>
+          <img src={book} alt="quote-book" className="quote-book" />
         </div>
-        <img src={book} alt="quote-book" className="quote-book" />
-        <img
-          src={greenSeperator}
-          alt="seperator-green"
-          className="seperator-green"
-        />
+        <svg
+          className="quote-wave"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path d="M0,256L120,234.7C240,213,480,171,720,181.3C960,192,1200,256,1320,288L1440,320L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path>
+        </svg>
       </div>
       <div className="features-container">
         <div className="single-feature-container">
