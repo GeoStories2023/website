cb1b1a256c58da64e9d701b90195c3352c3d766d
zwergt1 <104351222+zwergt1@users.noreply.github.com>
2023-04-26 09:39:51 +0200

    create premium ad

---
+++ src/components/premium-advertisement/PremiumAdvertisement.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "@/style/PremiumAdvertisement.scss";
+
+function PremiumAdvertisement() {
+    return (
+        <div className="premium-advertisement-container">
+            <h1>Premium Ad</h1>
+        </div>
+    )
+}
+
+export default PremiumAdvertisement;
