703f802ca7fa8931145dc6ca8f7308c39f9c1212
zwergt1 <104351222+zwergt1@users.noreply.github.com>
2023-04-26 11:02:39 +0200

    Add Premium-Ad Banner and Small

--- src/components/premium-advertisement/PremiumAdvertisementSmall.tsx
+++ src/components/premium-advertisement/PremiumAdvertisementSmall.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "@/style/PremiumAdvertisementSmall.scss"
+import premiumLogo from "@/assets/premium_globus.svg";
+import { useNavigate } from "react-router-dom";
 
 function PremiumAdvertisementSmall() {
+    const navigate = useNavigate();
     return(
-        <div className="premium-advertisement-small-container">
-            <h1>Premium Add Small</h1>
+        <div className="premium-advertisement-small-container" 
+        onClick={() => {
+                      navigate("/premium");
+                    }}>
+            <span>Get</span>
+            <img className="premium-logo" src={premiumLogo} alt="premiumLogo" />
+            <span>Premium</span>
         </div>
     )
 }
