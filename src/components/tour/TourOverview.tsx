22019694c5f325357087dceae53a3dd1473109ba
zwergt1 <104351222+zwergt1@users.noreply.github.com>
2023-05-03 09:03:55 +0200

    Create TourOverview

---
+++ src/components/tour/TourOverview.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "@/style/TourOverview.scss";
+import hamburg from "@/assets/img-dashboard-tours/hamburg.jpg";
+
+function TourOverview() {
+    return(
+        <div className="tour-overview-container">
+            <div className="tour-details">
+                <div className="tour-details-container">
+                    <img src="" alt="" className="tour-picture" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TourOverview;
