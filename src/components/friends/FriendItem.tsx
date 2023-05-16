9ad77ead204d687ef5587907f93c8a850240c0e0
noah <noah.thiering@web.de>
2023-05-16 13:49:53 +0200

    Add Friend delete icon

--- src/components/friends/FriendItem.tsx
+++ src/components/friends/FriendItem.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import "@/style/FriendItem.scss";
 import { IoPersonCircle as ProfilePicture } from "react-icons/io5";
 import { BsFillCircleFill as Circle } from "react-icons/bs";
-import Friends from "./Friends";
+import { FetchApi } from "@/FetchApi";
 
 function FriendItem({ friend }: { friend: any }) {
+  const accessToken = localStorage.getItem("accessToken") ?? "";
   let colorStatus = "";
 
   function handleStatusColor() {
@@ -30,8 +31,21 @@ function FriendItem({ friend }: { friend: any }) {
 
   handleStatusColor();
 
+  function handleRemoveFriend() {
+    console.log("remove friend");
+    FetchApi.delete(`/users/friends/${friend.uid}`, accessToken)
+  }
+
+
   return (
     <div className="friend-item">
+      <div className="friend-item-header">
+        <div className="friend-item-remove" onClick={handleRemoveFriend}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+          </svg>
+        </div>
+      </div>
       <div className="friend-item-container">
         <div className="friend-item-image">
           <ProfilePicture className="status-image" size={60} color="darkblue" />
