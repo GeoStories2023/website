28a0d45244274390506dca96061c4bba9ccc021c
zwergt1 <104351222+zwergt1@users.noreply.github.com>
2023-05-17 21:31:43 +0200

    Add TravelDiary

--- src/components/traveldiary/TravelDiary.tsx
+++ src/components/traveldiary/TravelDiary.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import "@/style/TravelDiary.scss";
+import { BsFillImageFill as Image } from "react-icons/bs";
 
 function TravelDiary() {
+  const tourName = "Bremer Altstadt Tour";
+  let j = 8;
+
   return (
     <div className="travel-diary-container">
+      <div className="travel-diary-header">
+        <span className="travel-diary-title">Reisetagebuch</span>
+        <span className="travel-diary-tour">{tourName}</span>
+      </div>
+      <div className="travel-diary-text-container">
+        <textarea className="travel-diary-text" name="tourText" id="tourText" placeholder="Trage hier deinen Eintrag ein."/>
+        <div className="seperator"></div>
+      </div>
+      <div className="travel-diary-image-container">
+        {Array.from(Array(8), (e, i) => {
+          return <Image size={250} key={i} />
+        })}
+      </div>
     </div>
   );
 }
