3b143258573a4a0f63a36bf4014b9eb7e560575d
Noah4ever <noah.thiering@web.de>
2023-06-01 11:01:42 +0200

    Add statistics data from rest

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -1,102 +1,126 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "@/style/Statistics.scss";
 import StatisticItem from "./StatisticItem";
-import { IoEarth as Countries, IoPersonCircle as ProfilePicture } from "react-icons/io5";
+import {
+  IoEarth as Countries,
+  IoPersonCircle as ProfilePicture,
+} from "react-icons/io5";
 import { FaCity as Cities } from "react-icons/fa";
 import { MdPhotoCamera as Tours } from "react-icons/md";
+import { FetchApi } from "@/FetchApi";
+import { Tour } from "@prisma/client";
 
-function Statistics(){
+function Statistics() {
+  const accessToken = localStorage.getItem("accessToken") ?? "";
+  const [user, setUser] = useState < any > ();
+  const [userStatistics, setUserStatistics] = useState < any > ([]);
+  const [error, setError] = useState < any > (null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const statistics = [
-        {
-            name: "Länder",
-            amount: 5,     
-            icon: <Countries size={90} />,
-            id: 1,   
-        },
-        {
-            name: "Städte",
-            amount: 15,     
-            icon: <Cities size={90} />,
-            id: 2,
-        },
-        {
-            name: "Touren",
-            amount: 30,     
-            icon: <Tours size={90} />,
-            id: 3,
-        },
-        {
-            name: "Länder",
-            amount: 5,     
-            icon: <Countries size={90} />,
-            id: 4,   
-        },
-        {
-            name: "Städte",
-            amount: 15,     
-            icon: <Cities size={90} />,
-            id: 5,
-        },
-        {
-            name: "Touren",
-            amount: 30,     
-            icon: <Tours size={90} />,
-            id: 6,
-        },
-    ];
+  useEffect(() => {
+    FetchApi.get(`users`, accessToken)
+      .then((response) => {
+        setUser(response);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+    FetchApi.get(`users/statistics`, accessToken)
+      .then((response) => {
+        setUserStatistics(response);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
 
-    return (
-        <div className="statistics-container">
-            <div className="">
-                <span className="statistics-title">Statistiken</span>
-                <div className="statistics-items">
-                    <div className="statistics-items-container">
-                        <section className="statistic-cards">
-                            <div className="row">
-                                <div className="col-12 col-lg-8">
-                                    <div className="row">
-                                        {statistics.map((statistics: any) => {
-                                            return <StatisticItem key={statistics.id} statistics={statistics} />;
-                                        })}
-                                    </div>
-                                </div>
-                                <div className="col-12 col-lg-4">
-                                    <div className="statistics-friends">
-                                        <div className="statistics-friends-container">
-                                            <table>
-                                                <tr>
-                                                    <th>Platz</th>
-                                                    <th>Name</th>
-                                                    <th>Story-Points</th>
-                                                </tr>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>Lena</td>
-                                                    <td>40.000</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>2</td>
-                                                    <td>Noah</td>
-                                                    <td>30.000</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>3</td>
-                                                    <td>Nils</td>
-                                                    <td>25.000</td>
-                                                </tr>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
+  const statistics = [
+    {
+      name: "Countries",
+      amount: userStatistics.visitedCountries
+        ? Object.keys(userStatistics?.visitedCountries).length
+        : 0,
+      icon: <Countries size={90} />,
+      id: 1,
+    },
+    {
+      name: "Cities",
+      amount: userStatistics.visitedCities
+        ? Object.keys(userStatistics?.visitedCities).length
+        : 0,
+      icon: <Cities size={90} />,
+      id: 2,
+    },
+    {
+      name: "Tours",
+      amount:
+        user?.startedTours.filter((tour: Tour) => tour.isCompleted).length ?? 0,
+      icon: <Tours size={90} />,
+      id: 3,
+    },
+  ];
+
+  return (
+    <div className="statistics-container">
+      <div className="">
+        <span className="statistics-title">Statistics</span>
+        <div className="statistics-items">
+          <div className="statistics-items-container">
+            <section className="statistic-cards">
+              <div className="row">
+                <div className="col-12 col-lg-8">
+                  <div className="row ">
+                    {statistics.map((statistics: any) => {
+                      return (
+                        <StatisticItem
+                          key={statistics.id}
+                          statistics={statistics}
+                        />
+                      );
+                    })}
+                  </div>
                 </div>
-            </div>
+                <div className="col-12 col-lg-4">
+                  <div className="statistics-friends">
+                    <div className="statistics-friends-container">
+                      <table>
+                        <tbody>
+                          <tr>
+                            <th>Place</th>
+                            <th>Name</th>
+                            <th>Story-Points</th>
+                          </tr>
+                          <tr>
+                            <td>1</td>
+                            <td>Lena</td>
+                            <td>40.000</td>
+                          </tr>
+                          <tr>
+                            <td>2</td>
+                            <td>Noah</td>
+                            <td>30.000</td>
+                          </tr>
+                          <tr>
+                            <td>3</td>
+                            <td>Nils</td>
+                            <td>25.000</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
-    );
-
+      </div>
+    </div>
+  );
 }
 
 export default Statistics;
